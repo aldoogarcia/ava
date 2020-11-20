70bf2081985bf2dec3c6ba6f6387fbc8de47d7c9
aldoogarcia <aldoredcreo@gmail.com>
2020-11-19 18:49:11 -0600

    version1.1

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$conexion = new mysqli("162.241.60.169","avaongco_formu","fj8SW^lPUf8I","avaongco_form",);
+	$conexion = new mysqli("","avaongco_formu","","avaongco_form",);
 	if(mysqli_connect_errno()){
 		echo 'Conexion fallida: ', mysqli_connect_error();
 		exit();
